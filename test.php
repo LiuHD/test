60cc8a4724fac97cf672b8566b6853a56324d5d0
LiuHD <liuhd@olive.fm>
2016-03-15 10:37:55 +0800

    fighting

--- test.php
+++ test.php
@@ -4,10 +4,10 @@ require_once __DIR__.'/bootstrap.php';
 use Validator\Check;
 use Test\B;
 use Test\C;
+
+header('type:text/txt');
 $check=new Check();
 $a=new A();
 $a->congratulation();
-echo PATH_SEPARATOR.PHP_EOL;
-echo get_include_path();
 B::b();
 C::c();
