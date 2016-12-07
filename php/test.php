ffbceb042fbf18932a64fadc9f2f2267cfc51d77
Martin <liuhuidong8230@163.com>
2016-12-07 23:19:11 +0800

    nothing

--- php/test.php
+++ php/test.php
@@ -3,9 +3,8 @@ include_once __DIR__ . '/lib/google.php';
 include_once __DIR__ . '/lang.php';
 ini_set('display_errors', 1);
 error_reporting(E_ALL | E_NOTICE);
-header('text/html');
 global $LANG;
-echo count($LANG);
+$i = 0;
 foreach ($LANG as $k => $v){
-    echo $k.'<br>';
+    echo ++$i. ' => '. $v . PHP_EOL;
 }
